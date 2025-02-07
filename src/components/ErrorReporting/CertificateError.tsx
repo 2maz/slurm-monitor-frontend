7700eb092866c28cd006c20a1896932054042cd7
Thomas Roehr <roehr@simula.no>
2025-02-07 11:09:22 +0100

    certificate error: fix missing whitespace

--- src/components/ErrorReporting/CertificateError.tsx
+++ src/components/ErrorReporting/CertificateError.tsx
@@ -4,7 +4,7 @@ const CertificateError = () => {
   const backendUrl = useAppState((state) => state.backendUrl);
   return <div>
       This might be a self-signed certificate issue.
-      Go to <a href={backendUrl}>{backendUrl}</a> 
+      Go to <a href={backendUrl}>{backendUrl} </a>
       and if you are warned about a self-signed certificate exception, add a permanent exception for this site.
     </div>
 }
