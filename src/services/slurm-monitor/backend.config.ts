bb659dd402f5be1ecdfb10133811ccbf01ab0412
Thomas Roehr <roehr@simula.no>
2025-02-28 16:53:59 +0100

    Change default backend url

--- src/services/slurm-monitor/backend.config.ts
+++ src/services/slurm-monitor/backend.config.ts
@@ -1,6 +1,6 @@
 import { CanceledError } from "axios";
 
-export const DEFAULT_BACKEND_URL = "https://srl-login3.ex3.simula.no:12000";
+export const DEFAULT_BACKEND_URL = "https://naic-monitor.simula.no:12000";
 export const MONITOR_API_PREFIX = "/api/v1/monitor/";
 
 export const MONITOR_BASE_URL = DEFAULT_BACKEND_URL + MONITOR_API_PREFIX
