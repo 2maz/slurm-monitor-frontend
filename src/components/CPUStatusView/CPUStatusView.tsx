81ee0200fa8bd0bc27699c1972193171b9a57f65
Thomas Roehr <roehr@simula.no>
2025-03-20 20:37:56 +0100

    CPUStatusView: fix charting

--- src/components/CPUStatusView/CPUStatusView.tsx
+++ src/components/CPUStatusView/CPUStatusView.tsx
@@ -1,8 +1,10 @@
 
 import { LineChart, Line, Tooltip, XAxis, YAxis, Legend, CartesianGrid } from 'recharts';
-import moment from "moment";
 import { BarLoader } from 'react-spinners';
 import useCPUStatus from '../../hooks/useCPUStatus';
+import { DateTime } from 'luxon';
+import useNodesInfo from '../../hooks/useNodesInfos';
+import { JSX } from 'react';
 
 interface Props {
   nodename: string;
@@ -13,6 +15,7 @@ interface Props {
 }
 
 const CPUStatusView = ({nodename, start_time_in_s, end_time_in_s, resolution_in_s, refresh_interval_in_s = 1000*60} : Props) => {
+    const { data : nodes_info, error: nodes_info_error, isLoading: nodes_info_isLoading } = useNodesInfo()
     const {data: nodes_processes, error, isLoading, isSuccess } = useCPUStatus({
       nodename: nodename,
       start_time_in_s: start_time_in_s,
@@ -20,15 +23,20 @@ const CPUStatusView = ({nodename, start_time_in_s, end_time_in_s, resolution_in_
       resolution_in_s: resolution_in_s
     }, refresh_interval_in_s
   )
-  if(isLoading)
+  if(isLoading || nodes_info_isLoading)
     return <BarLoader />
+
+  if(nodes_info_error)
+    return "Failed loading processes data for {nodename}"
 
   if(error)
     return "Failed loading processes data for {nodename}"
 
+  const cpuCount = nodes_info ? nodes_info[nodename].cpus.count : 1
+
   const elements : JSX.Element[] = []
   if(isSuccess) {
-    Object.keys(nodes_processes).map((process_id: string) => (
+    Object.keys(nodes_processes).map((process_id: string) => {
       elements.push(
             <div key={process_id + "cpu_status"}>
             <h4>Node: {nodename}</h4>
@@ -36,10 +44,13 @@ const CPUStatusView = ({nodename, start_time_in_s, end_time_in_s, resolution_in_
               <LineChart width={300} height={250} data={nodes_processes[process_id].data}>
                 <Line yAxisId="1" type="monotone" dataKey="cpu_percent" stroke="#8884d8"/>
                 <CartesianGrid strokeDasharray="3 3"/>
-                <XAxis dataKey="timestamp" tickFormatter={timestamp => moment(timestamp as Date).format("HH:mm")} />
-                <YAxis orientation="left" domain={[0, 100]} yAxisId="1"
+                <XAxis dataKey="timestamp" tickFormatter={timestamp => DateTime.fromISO(timestamp as string).toFormat("HH:mm")} />
+                <YAxis 
+                  orientation="left"
+                  domain={[0, cpuCount*100]}
+                  yAxisId="1"
                   label={{
-                    value: `percentage (%)`,
+                    value: `percentages (%)`,
                     style: { textAnchor: 'middle' },
                     angle: -90,
                     position: 'left',
@@ -52,7 +63,7 @@ const CPUStatusView = ({nodename, start_time_in_s, end_time_in_s, resolution_in_
             </div>
             </div>
           )
-    ))
+    })
   }
   return (<>{elements}</>)
 }
