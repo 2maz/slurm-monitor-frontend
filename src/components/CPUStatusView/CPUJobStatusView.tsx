7f8ce9b7520bf95d45b8a1c48624855159140bb1
Thomas Roehr <roehr@simula.no>
2025-10-17 21:10:14 +0200

    CPUStatusView: reactivate reference line for allocated cpus

--- src/components/CPUStatusView/CPUJobStatusView.tsx
+++ src/components/CPUStatusView/CPUJobStatusView.tsx
@@ -1,8 +1,11 @@
-import { LineChart, Line, Tooltip, XAxis, YAxis, Legend, CartesianGrid } from 'recharts';
+import { LineChart, Line, Tooltip, XAxis, YAxis, Legend, CartesianGrid, ReferenceLine } from 'recharts';
 import { BarLoader } from "react-spinners";
 import { DateTime } from 'luxon';
 
 import useJobProcessesStatus from "../../hooks/useJobProcessesStatus";
+import useJobStatus from "../../hooks/useJobStatus";
+
+
 import { JSX } from 'react';
 
 interface Props {
@@ -14,6 +17,7 @@ interface Props {
 }
 
 const CPUJobStatusView = ({job_id, start_time_in_s, end_time_in_s, resolution_in_s, refresh_interval_in_s = 1000*60} : Props) => {
+  const { data : job_status, error: job_status_error, isLoading: job_status_isLoading } = useJobStatus(job_id, refresh_interval_in_s)
   const { data, error, isLoading } = useJobProcessesStatus({
       job_id: job_id,
       start_time_in_s: start_time_in_s,
@@ -22,39 +26,44 @@ const CPUJobStatusView = ({job_id, start_time_in_s, end_time_in_s, resolution_in
     },
     refresh_interval_in_s
   )
-  if(isLoading)
+  if(isLoading || job_status_isLoading)
     return <BarLoader />
 
   if(error) {
     const details = error.response as { data: { detail: string }}
     return "Failed to retrieve status data for job " + job_id + " -- " + details.data.detail
   }
+  if(job_status_error) {
+    const details = job_status_error.response as { data: { detail: string }}
+    return "Failed to retrieve status data for job " + job_id + " -- " + details.data.detail
+  }
 
   const elements : JSX.Element[] = []
 
-  //<ReferenceLine yAxisId="1" y={allocated_cpus*100} label={allocated_cpus + " cpu(s) allocated"} stroke="red" strokeDasharray="40 150"/>
-  if(data && data.length > 0) {
+  if(job_status && data && data.length > 0) {
     Object.keys(data[0].nodes).map((nodename: string, index: number) => {
       const node_data = data[0].nodes[nodename].cpu_memory
       elements.push(
             <div key={index}>
               <h4>Node: {nodename}</h4>
               <div className="mx-5" key="{job_id}-accumulated" >
-                <LineChart width={400} height={300} data={node_data}>
+                <LineChart width={600} height={300} margin={{left: 50, right: 50}}data={node_data}>
                   <Line yAxisId="1" type="monotone" dataKey="cpu_util" name="CPU Utilization (%)" stroke="#8884d8"/>
                   <Line yAxisId="1" type="monotone" dataKey="memory_util" name="Memory Utilization (%)" stroke="#888400"/>
                   <Line yAxisId="2" type="monotone" dataKey="processes_avg" name="Average number of processes" stroke="#008400"/>
                   <CartesianGrid strokeDasharray="3 3"/>
                   <XAxis dataKey="time" tickFormatter={timestamp => DateTime.fromISO(timestamp as unknown as string, { zone: 'utc'}).toFormat("HH:mm")} />
-                  <YAxis orientation="left" domain={[0, 100]} yAxisId="1"
+                  <YAxis orientation="left" domain={[0, job_status.requested_cpus*100*1.2]} yAxisId="1"
                     label={{
                       value: `percentage (%)`,
                       style: { textAnchor: 'middle' },
                       angle: -90,
                       position: 'left',
-                      offset: 0,
+                      offset: 25,
                     }}
+                    tickFormatter={(value: number) => value.toFixed(2)}
                   />
+                  <ReferenceLine yAxisId="1" y={job_status.requested_cpus*100} label={{ value: job_status.requested_cpus + " cpu(s) allocated", fill: 'black', style: { fontWeight: 'bolder' }}} stroke="red" strokeDasharray="80 220"/>
                   <YAxis orientation="right" domain={[0, 100]} yAxisId="2"
                     label={{
                       value: `count`,
--- src/hooks/useJobStatus.ts
+++ src/hooks/useJobStatus.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useMonitorEndpoint from "./useMonitorEndpoint";
 import Job from "../components/JobsView/Job";
+import { AxiosError } from "axios";
 
 const useJobStatus = (job_id: number, refresh_interval_in_s: number = 60) => {
   const { endpoint } = useMonitorEndpoint("/jobs/" + job_id);
@@ -14,7 +15,7 @@ const useJobStatus = (job_id: number, refresh_interval_in_s: number = 60) => {
       })
   };
 
-  return useQuery<Job, Error>({
+  return useQuery<Job, AxiosError>({
     queryKey: ["job_status", job_id],
     queryFn: fetchStatus,
     refetchInterval: refresh_interval_in_s*1000, // default refresh every minute
