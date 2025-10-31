6863cd818c7f4aeb1bfdba9d7c5ac007161c8219
Thomas Roehr <roehr@simula.no>
2025-10-31 23:38:08 +0100

    Allow .env based configuration of RESTAPI endpoint

--- src/services/slurm-monitor/backend.config.ts
+++ src/services/slurm-monitor/backend.config.ts
@@ -1,6 +1,8 @@
 import { CanceledError } from "axios";
 
-export const DEFAULT_BACKEND_URL = "https://naic-monitor.simula.no:12001";
+const SLURM_MONITOR_RESTAPI_PORT = (import.meta.env.SLURM_MONITOR_RESTAPI_PORT || 12200) as number;
+const SLURM_MONITOR_RESTAPI_HOST = (import.meta.env.SLURM_MONITOR_RESTAPI_HOST || "https://naic-monitor.simula.no") as string;
+export const DEFAULT_BACKEND_URL = SLURM_MONITOR_RESTAPI_HOST + ":" + SLURM_MONITOR_RESTAPI_PORT;
 export const MONITOR_API_PREFIX = "/api/v2/"
 
 export const MONITOR_BASE_URL = DEFAULT_BACKEND_URL + MONITOR_API_PREFIX
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ["srl-login3.ex3.simula.no"]
+    allowedHosts: [
+      "srl-login3.ex3.simula.no",
+      "naic-monitor.simula.no",
+      "localhost"
+    ]
   },
   plugins: [react()],
+  envPrefix: 'SLURM_MONITOR_'
 })
